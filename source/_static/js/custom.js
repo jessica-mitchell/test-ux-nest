79093edb2c9b4e2196fc722d2cc0b2a8433bf8a4
Jessica Mitchell <j.mitchell@fz-juelich.de>
2023-06-18 23:48:10 +0200

    the javascript that makes the popups work and svg clickable

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const svgElements = document.querySelectorAll("#mySvg .clickable");
-  const popups = document.querySelectorAll(".popuptext");
+// Generate popup. Using eventlisteners for click events, rather than 'onclick'
+// because: https://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick
 
+// Ensuring page content is loaded before executing
+document.addEventListener("DOMContentLoaded", function() {
+  // All svg elements of interest are tagged with class = 'clickable'and an id like 'neuron1'
+  const svgElements = document.querySelectorAll("#mySvg .clickable");
+  // Find all the popups (note: the popups are embeded HTML directly in rst
+  const popups = document.querySelectorAll(".popuptext");
+  // add click event listener and get the 'ids'
   svgElements.forEach(function(svgElement) {
     svgElement.addEventListener("click", function() {
       const svgElementId = svgElement.getAttribute("id");
@@ -13,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
-
+  // if a click event happens outside the svg area hide popups
   document.addEventListener("click", function(event) {
     if (!event.target.closest("#mySvg")) {
       popups.forEach(function(popup) {
@@ -21,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     }
   });
-
+  // ensure only one popup is shown at a time
   function togglePopup(popup) {
     if (popup.classList.contains("visible")) {
       hidePopup(popup);
@@ -44,15 +50,13 @@ document.addEventListener("DOMContentLoaded", function() {
       hidePopup(popup);
     });
   }
-
+  // look for matching terms in the popup id and svg element so correct image is used
+  // Using neuron, synapse, device
   function getMatchingPopupId(svgElementId) {
     const idWord = svgElementId.replace(/[0-9]+$/, "");
     const matchingPopup = Array.from(popups).find(function(popup) {
       return popup.id === idWord;
     });
-
-    console.log("ID Word:", idWord);
-    console.log("Matching Popup:", matchingPopup);
 
     return matchingPopup ? matchingPopup.id : null;
   }
